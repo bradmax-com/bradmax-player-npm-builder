a695872cfdca4e704ae6777e4586ef8f89a47649
mthk <>
2018-03-19 15:13:56 +0000

    added 'bump:(major/minor/patch)' command

--- src/bradmax.js
+++ src/bradmax.js
@@ -3,22 +3,30 @@
 'use strict';
 
 const _cli = require('commander');
+const _semver = require('semver')
 const _log = require('./util/logger');
 const _help = require('./util/helper');
 const _file = require('./util/file');
 const _server = require('./util/server');
+const _run = require('./util/runner');
 //
-const PACKAGE = require('./../package.json');
 const SETUP = require('./../setup.json');
-const PACKAGE_PLAYER_JS = require(_help.root(`${SETUP['bradmax-player-js']}/package.json`));
-const PACKAGE_PLAYER_AG = require(_help.root(`${SETUP['bradmax-player-ag']}/package.json`));
-const PACKAGE_PLAYER_NG = require(_help.root(`${SETUP['bradmax-player-ng']}/package.json`));
-const PACKAGE_PLAYER_RX = require(_help.root(`${SETUP['bradmax-player-rxjs']}/package.json`));
+const PACKAGE_PATH = _help.root('package.json');
+const PACKAGE_PLAYER_JS_PATH = _help.root(`${SETUP['bradmax-player-js']}/package.json`);
+const PACKAGE_PLAYER_AG_PATH = _help.root(`${SETUP['bradmax-player-ag']}/package.json`);
+const PACKAGE_PLAYER_NG_PATH = _help.root(`${SETUP['bradmax-player-ng']}/package.json`);
+const PACKAGE_PLAYER_RX_PATH = _help.root(`${SETUP['bradmax-player-rxjs']}/package.json`);
 //
 const _playerJs = require('./package/player');
 const _playerAg = require('./package/player-ag');
 const _playerNg = require('./package/player-ng');
 const _playerRx = require('./package/player-rx');
+//
+const PACKAGE = require(PACKAGE_PATH);
+const PACKAGE_PLAYER_JS = require(PACKAGE_PLAYER_JS_PATH);
+const PACKAGE_PLAYER_AG = require(PACKAGE_PLAYER_AG_PATH);
+const PACKAGE_PLAYER_NG = require(PACKAGE_PLAYER_NG_PATH);
+const PACKAGE_PLAYER_RX = require(PACKAGE_PLAYER_RX_PATH);
 //
 const PLAYER_NAMES = ["gorilla", "mole", "snake", "zebra"];
 const TGZ_DIR = _help.root(`dist`);
@@ -35,6 +43,9 @@ _cli.version(PACKAGE.version)
 	.option('-s, --silent', 'suppress log messages.')
 	.option('-d, --debug', 'run debug compilation.');
 /// commands
+_cli.command('bump:major').description(`bump packages major version.`).action(() => playerBump('major'));
+_cli.command('bump:minor').description(`bump packages minor version.`).action(() => playerBump('minor'));
+_cli.command('bump:patch').description(`bump packages patch version.`).action(() => playerBump('patch'));
 _cli.command('player:js').description(`build ${PACKAGE_PLAYER_JS.name} package.`).action(playerJs);
 _cli.command('player:ag').description(`build ${PACKAGE_PLAYER_AG.name} package.`).action(playerAg);
 _cli.command('player:ng').description(`build ${PACKAGE_PLAYER_NG.name} package.`).action(playerNg);
@@ -46,6 +57,54 @@ _cli.command('player:server:stop').description(`stops mock distribution server f
 _cli.parse(process.argv);
 
 ///// PROGRAMS
+
+/// player js
+async function playerBump(release) {
+	_log.log('START');
+
+	try {
+		_help.start(PACKAGE);
+		_log.task(`bump ${release}`);
+		const valid = _semver.valid(PACKAGE.version);
+		if (valid == null)
+			throw `${PACKAGE.version} is invalid semver !`;
+
+		PACKAGE.version =
+			PACKAGE_PLAYER_JS.version =
+			PACKAGE_PLAYER_AG.version =
+			PACKAGE_PLAYER_NG.version =
+			PACKAGE_PLAYER_RX.version =
+			_semver.inc(PACKAGE.version, release);
+
+		_log.verbose(PACKAGE.version, PACKAGE.name);
+		_log.verbose(PACKAGE_PLAYER_JS.version, PACKAGE_PLAYER_JS.name);
+		_log.verbose(PACKAGE_PLAYER_AG.version, PACKAGE_PLAYER_AG.name);
+		_log.verbose(PACKAGE_PLAYER_NG.version, PACKAGE_PLAYER_NG.name);
+		_log.verbose(PACKAGE_PLAYER_RX.version, PACKAGE_PLAYER_RX.name);
+		_log.done();
+
+		_log.task(`write packages`);
+		let tasks = [];
+		tasks.push(_file.write(PACKAGE_PATH, JSON.stringify(PACKAGE, null, '\t')));
+		tasks.push(_file.write(PACKAGE_PLAYER_JS_PATH, JSON.stringify(PACKAGE_PLAYER_JS, null, '\t')));
+		tasks.push(_file.write(PACKAGE_PLAYER_AG_PATH, JSON.stringify(PACKAGE_PLAYER_AG, null, '\t')));
+		tasks.push(_file.write(PACKAGE_PLAYER_NG_PATH, JSON.stringify(PACKAGE_PLAYER_NG, null, '\t')));
+		tasks.push(_file.write(PACKAGE_PLAYER_RX_PATH, JSON.stringify(PACKAGE_PLAYER_RX, null, '\t')));
+		await _run.parallel(tasks);
+		_log.done();
+
+	} catch (e) {
+		_log.error(e);
+		_help.stop(PACKAGE);
+		_log.log('EXIT:ON:ERROR');
+		process.exit(1);
+	} finally {
+		_help.stop(PACKAGE);
+		_log.log('EXIT');
+		process.exit(0);
+	}
+
+}
 
 /// player js
 async function playerJs() {
